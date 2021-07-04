49161e35b299c419413e5b05bf4bbcda2ef81b3c
yisian <64526036+toasterclock@users.noreply.github.com>
2021-07-04 07:38:23 +0000

    Major Update: Changed entirely to bot commands rather than waiting for on_message. In development: Setting your study voice channel (for admins) and the study voice channel time logging

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from replit import db
 #Testing Replit DB but any obviously you can use any DB of your choice or just simply use a .txt file like i did
 from keep_alive import keep_alive
 import discord
+from discord.ext import commands
 import os
 import time
 #import json
 #Made by @toasterclock
-client = discord.Client()
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="$", intents=intents, activity= discord.Activity(type=discord.ActivityType.listening, name="$help"))
+bot.remove_command('help')
 
 #allTodo = json.load(open("todolist.txt"))
 allTimer = {}
@@ -82,91 +85,94 @@ def time_convert(seconds):
       return str(seconds) + "s"
 
 #On Ready - prints that its ready
-@client.event
+@bot.event
 async def on_ready():
-    print('Connection successful,  {0.user}'.format(client))
-    client.change_presence(game=discord.Game(name='$help'))
+    print('Connection successful,  {0.user}'.format(bot))
+
 #Under Development - On Bot Join: Send Messages
-@client.event
-async def on_guild_join(message):
+@bot.event
+async def on_guild_join(ctx):
   embedBotJoinServer = discord.Embed(
         title="Welcome to ProductivityBot", colour = discord.Colour.random())
   embedBotJoinServer.add_field(name="First-time setup", value="Please use ```$set studyvc (channelname)``` to set your desired Study Voice Channel \n Use $help for command usage.")
   embedBotJoinServer.set_footer(text='Enjoy your stay!')
-  await message.system_channel.send(content = None, embed = embedBotJoinServer)
+  await ctx.system_channel.send(content = None, embed = embedBotJoinServer)
   print("First-time setup sent")
 #All Commands reside below this comment
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+
+
+async def on_message(ctx):
+    if ctx.author == bot.user:
         return
 
+@bot.command()
+async def help(ctx):
+    embedHelp = discord.Embed(
+    title="How to use the Productivity Bot", 
+    description="Some useful commands",
+    colour=discord.Colour.blue())
+    embedHelp.add_field(name="$startwatch", value="Starts Stopwatch")
+    embedHelp.add_field(name="$stopwatch", value="Ends Stopwatch")
+    embedHelp.add_field(name="$todos", value="Personal Todo List \n $todos add (name) \n $todos remove (number)\n $todos clear" ,inline=True)
+    embedHelp.add_field(name="$hw", value="Server Todo List \n $hw add (name) \n $hw remove (number) \n $hw clear")
+    await ctx.channel.send(content=None, embed=embedHelp)
 
+@bot.command()
+async def startwatch(ctx):
+    timerAuthor = str(ctx.author.id)
+    user = await bot.fetch_user(timerAuthor)
+    if timerAuthor in allTimer:
+      embedStartwatch = discord.Embed(
+      title="❌ "+ "You still have an active stopwatch running", description="Unable to start more than one stopwatch",
+      colour=discord.Colour.red())
+      embedStartwatch.set_author(name=user, icon_url=user.avatar_url)
+      await ctx.channel.send(content=None, embed=embedStartwatch)
+    else:
+      newTimer(timerAuthor)
+      embedStartwatch = discord.Embed(
+      title="<:datree:858669536885997588>"+ "Stopwatch started", 
+      description="To end it, type $stopwatch",
+      colour=discord.Colour.red())
+      embedStartwatch.set_author(name=user, icon_url=user.avatar_url)
+      await ctx.channel.send(content=None, embed=embedStartwatch)
 
-    if message.content.lower() == "$help":
-        embedHelp = discord.Embed(
-        title="How to use the Productivity Bot", 
-        description="Some useful commands",
-        colour=discord.Colour.blue())
-        embedHelp.add_field(name="$startwatch", value="Starts Stopwatch")
-        embedHelp.add_field(name="$stopwatch", value="Ends Stopwatch")
-        embedHelp.add_field(name="$todos", value="$todos add (name) \n $todos remove (number)\n $todos clear" ,inline=True)
-        embedHelp.add_field(name="$hw", value="$hw add (name) \n $hw remove (number) \n $hw clear")
-        await message.channel.send(content=None, embed=embedHelp)
-
-    if message.content.startswith("$startwatch"):
-        timerAuthor = str(message.author.id)
-        user = await client.fetch_user(timerAuthor)
-        if timerAuthor in allTimer:
-          embedStartwatch = discord.Embed(
-          title="❌ "+ "You still have an active stopwatch running", description="Unable to start more than one stopwatch",
-          colour=discord.Colour.red())
-          embedStartwatch.set_author(name=user, icon_url=user.avatar_url)
-          await message.channel.send(content=None, embed=embedStartwatch)
-        else:
-          newTimer(timerAuthor)
-
-          embedStartwatch = discord.Embed(
-          title="<:datree:858669536885997588>"+ "Stopwatch started", 
-          description="To end it, type $stopwatch",
-          colour=discord.Colour.red())
-          embedStartwatch.set_author(name=user, icon_url=user.avatar_url)
-
-          await message.channel.send(content=None, embed=embedStartwatch)
-
-    if message.content.startswith("$stopwatch"):
-
-      timerAuthor = str(message.author.id)
-      user = await client.fetch_user(timerAuthor)
-      embedStopwatch = discord.Embed(title="<a:pogoslide:858669948880551966> " + "Time Spent: "+ stopTimer(timerAuthor),
-      colour=discord.Colour.green())
-      embedStopwatch.set_author(name=user, icon_url=user.avatar_url)
-      embedStopwatch.set_footer(text= "Good job!")
-      await message.channel.send(content=None, embed=embedStopwatch)
-      
-    if message.content.lower() == '$todos':
-      authorSent = str(message.author.id)
-      user = await client.fetch_user(authorSent)
+@bot.command()
+async def stopwatch(ctx):
+    timerAuthor = str(ctx.author.id)
+    user = await bot.fetch_user(timerAuthor)
+    embedStopwatch = discord.Embed(title="<a:pogoslide:858669948880551966> " + "Time Spent: "+ stopTimer(timerAuthor),
+    colour=discord.Colour.green())
+    embedStopwatch.set_author(name=user, icon_url=user.avatar_url)
+    embedStopwatch.set_footer(text= "Good job!")
+    await ctx.channel.send(content=None, embed=embedStopwatch)
+  
+@bot.command()
+async def todos(ctx, *args):
+    args = list(args)
+    if len(args) == 0:
+      authorSent = str(ctx.author.id)
+      user = await bot.fetch_user(authorSent)
       if authorSent not in db.keys():
         addAllTodo(authorSent)
         embedTodos = discord.Embed(title="✅ "+ "Your Todo List has been created.", colour=discord.Colour.green())
         embedTodos.set_footer(text="This applies to first time users only")
         #json.dump(allTodo, open("todolist.txt",'w'))
-        
 
-        await message.channel.send(content=None, embed=embedTodos)
+        await ctx.send(content=None, embed=embedTodos)
       else:
         #json.dump(allTodo, open("todolist.txt",'w'))
         embedTodos = discord.Embed(title="📝 "+ "Your Todo List", description=authorTodo(authorSent), colour = discord.Colour.random())
         embedTodos.set_author(name=user, icon_url=user.avatar_url)
-        await message.channel.send(content=None, embed=embedTodos)
-        
-    #Individual Todo Lists
-    if message.content.startswith("$todos add"):
-      cmd = message.content[11:]
-      #Who Sent This?
-      authorSent = str(message.author.id)
-      user = await client.fetch_user(authorSent)
+        await ctx.send(content=None, embed=embedTodos)
+      #Individual Todo Lists
+
+    elif args[0] == "add":
+      cmd = ''
+      args.pop(0)
+      for arg in args:
+        cmd += ' ' + arg
+      authorSent = str(ctx.author.id) #Who Sent It
+      user = await bot.fetch_user(authorSent)
       #ensure that a key is created for each user
       if authorSent not in db.keys():
         addAllTodo(authorSent)
@@ -178,10 +184,10 @@ async def on_message(message):
         embedTodosAdd.set_author(name=user, icon_url=user.avatar_url)
 
 
-        await message.channel.send(embed=embedTodosAdd, content = None)
+        await ctx.send(embed=embedTodosAdd, content = None)
 
         embedTodos = discord.Embed(title="📝 "+ "Your Todo List", description=authorTodo(authorSent), colour=discord.Colour.random())
-        await message.channel.send(content=None, embed=embedTodos)
+        await ctx.send(content=None, embed=embedTodos)
 
       else:
 
@@ -193,20 +199,20 @@ async def on_message(message):
         embedTodosAdd.set_author(name=user, icon_url=user.avatar_url)
 
 
-        await message.channel.send(embed=embedTodosAdd, content = None)
+        await ctx.send(embed=embedTodosAdd, content = None)
 
         embedTodos = discord.Embed(title="📝 "+ "Your Todo List", description=authorTodo(authorSent), colour = discord.Colour.random())
-        await message.channel.send(content=None, embed=embedTodos)
+        await ctx.send(content=None, embed=embedTodos)
 
     #todos remove
-    if message.content.startswith('$todos remove'):
-      authorSent =  str(message.author.id) #Author ID
-      user = await client.fetch_user(authorSent) #Author Username
+    elif args[0] == "remove":
+      cmdremove = int(args[1])
+      authorSent =  str(ctx.author.id) #Author ID
+      user = await bot.fetch_user(authorSent) #Author Username
       #get rid of $todos remove in the string
-      cmdremove = message.content[14:]
       global whatWasRemoved
       # Read what is about to be removed from the Todo list
-      removeTodo(authorSent, int(cmdremove))
+      removeTodo(authorSent, cmdremove)
       aboutToRemove = removeTodo.whatWasRemoved
       #json.dump(allTodo, open("todolist.txt",'w')) // If you use a text file
       embedTodosRemove = discord.Embed(title = "❌ Removed "+ '"'+aboutToRemove+'"', colour=discord.Colour.red())
@@ -215,39 +221,52 @@ async def on_message(message):
       embedTodosRemove.set_author(name=user, icon_url=user.avatar_url)
 
       #async send back message
-      await message.channel.send(embed=embedTodosRemove, content = None)
+      await ctx.channel.send(embed=embedTodosRemove, content = None)
 
       embedTodos = discord.Embed(title="📝 "+ "Your Todo List", description=authorTodo(authorSent), colour=discord.Colour.random())
-      await message.channel.send(content=None, embed=embedTodos)
-    if message.content.lower() == "$todos clear":
-      authorSent =  str(message.author.id) #Author ID
-      user = await client.fetch_user(authorSent)
+      await ctx.channel.send(content=None, embed=embedTodos)
+
+      
+    elif args[0] == 'clear':
+      authorSent =  str(ctx.author.id) #Author ID
+      user = await bot.fetch_user(authorSent)
       removeAllTodo(authorSent)
       embedClearedTodo = discord.Embed(title="✅ Your Todo List has been cleared", colour=discord.Colour.green())
-      await message.channel.send(content=None, embed=embedClearedTodo)
+      await ctx.channel.send(content=None, embed=embedClearedTodo)
 
 
-    # Server List
-    if message.content.lower() == "$hw":
-      authorSent = str(message.guild.id)
+#In this case, variable user refers to ther server name instead of user name
+#userAdded stands for who added to the server's todo list
+#authorAdded stands for ID of person that added to the server's todo list
+#authorSent remains as server ID
+@bot.command()
+async def hw(ctx, *args):
+    args = list(args)
+    if len(args) == 0:
+      authorSent = str(ctx.guild.id)
+      user = ctx.guild.name
       if authorSent not in db.keys():
         addAllTodo(authorSent)
-        embedTodos = discord.Embed(title="✅ "+ "Server Todo List has been created", colour=discord.Colour.green())
+        embedTodos = discord.Embed(title=f"✅ {user}'s Todo List has been created", colour=discord.Colour.green())
         embedTodos.set_footer(text="This applies to first time users only")
         #json.dump(allTodo, open("todolist.txt",'w'))
-        
 
-        await message.channel.send(content=None, embed=embedTodos)
+        await ctx.send(content=None, embed=embedTodos)
       else:
         #json.dump(allTodo, open("todolist.txt",'w'))
-        embedTodos = discord.Embed(title="📝 "+ "Server Todo List", description=authorTodo(authorSent), colour = discord.Colour.random())
-        embedTodos.set_author(name=message.guild.name, icon_url=message.guild.icon_url)
-        await message.channel.send(content=None, embed=embedTodos)
-    
-    if message.content.startswith("$hw add"):
-      cmd = message.content[8:]
-      #Who Sent This?
-      authorSent = str(message.guild.id)
+        embedTodos = discord.Embed(title=f"📝 {user}'s Todo List", description=authorTodo(authorSent), colour = discord.Colour.random())
+        await ctx.send(content=None, embed=embedTodos)
+      #Individual Todo Lists
+
+    elif args[0] == "add":
+      cmd = ''
+      args.pop(0)
+      for arg in args:
+        cmd += ' ' + arg
+      authorSent = str(ctx.guild.id) #Guild 
+      authorAdded = str(ctx.author.id) #Who used the command
+      user = ctx.guild.name #Server Name
+      userAdded = await bot.fetch_user(authorAdded) #Who Added to Server List
       #ensure that a key is created for each user
       if authorSent not in db.keys():
         addAllTodo(authorSent)
@@ -255,14 +274,11 @@ async def on_message(message):
         #json.dump(allTodo, open("todolist.txt",'w')) // If you use a text file
         embedTodosAdd = discord.Embed(title = "✅ Added "+ '"'+cmd+'"', colour=discord.Colour.green())
 
+        embedTodosAdd.set_author(name=userAdded, icon_url=userAdded.avatar_url)
+        await ctx.send(embed=embedTodosAdd, content = None)
 
-        embedTodosAdd.set_author(name=message.guild.name, icon_url=message.guild.icon_url)
-
-
-        await message.channel.send(embed=embedTodosAdd, content = None)
-
-        embedTodos = discord.Embed(title="📝 "+ "Server Todo List", description=authorTodo(authorSent), colour=discord.Colour.random())
-        await message.channel.send(content=None, embed=embedTodos)
+        embedTodos = discord.Embed(title=f"📝 {user}'s Todo List", description=authorTodo(authorSent), colour=discord.Colour.random())
+        await ctx.send(content=None, embed=embedTodos)
 
       else:
 
@@ -270,74 +286,79 @@ async def on_message(message):
         #json.dump(allTodo, open("todolist.txt",'w')) // If you use a text file
         embedTodosAdd = discord.Embed(title = "✅ Added "+ '"'+cmd+'"', colour=discord.Colour.green())
 
+        embedTodosAdd.set_author(name=userAdded, icon_url=userAdded.avatar_url)
+        await ctx.send(embed=embedTodosAdd, content = None)
 
-        embedTodosAdd.set_author(name=message.guild.name, icon_url=message.guild.icon_url)
+        embedTodos = discord.Embed(title=f"📝 {user}'s Todo List", description=authorTodo(authorSent), colour=discord.Colour.random())
+        await ctx.send(content=None, embed=embedTodos)
 
-
-        await message.channel.send(embed=embedTodosAdd, content = None)
-
-        embedTodos = discord.Embed(title="📝 "+ "Server Todo List", description=authorTodo(authorSent), colour = discord.Colour.random())
-        await message.channel.send(content=None, embed=embedTodos)
-
-    if message.content.startswith('$hw remove'):
-      authorSent = str(message.guild.id) #Server ID
+    #todos remove
+    elif args[0] == "remove":
+      cmdremove = int(args[1])
+      authorSent = str(ctx.guild.id) #Server ID
+      authorRemoved = str(ctx.author.id) #User ID
+      user = ctx.guild.name #username
+      userRemoved = await bot.fetch_user(authorRemoved) #Server name
       #get rid of $todos remove in the string
-      cmdremove = message.content[11:]
       global whatWasRemoved
       # Read what is about to be removed from the Todo list
-      removeTodo(authorSent, int(cmdremove))
+      removeTodo(authorSent, cmdremove)
       aboutToRemove = removeTodo.whatWasRemoved
       #json.dump(allTodo, open("todolist.txt",'w')) // If you use a text file
       embedTodosRemove = discord.Embed(title = "❌ Removed "+ '"'+aboutToRemove+'"', colour=discord.Colour.red())
 
 
-      embedTodosRemove.set_author(name=message.guild.name, icon_url=message.guild.icon_url)
+      embedTodosRemove.set_author(name=userRemoved, icon_url=userRemoved.avatar_url)
 
       #async send back message
-      await message.channel.send(embed=embedTodosRemove, content = None)
+      await ctx.channel.send(embed=embedTodosRemove, content = None)
 
-      embedTodos = discord.Embed(title="📝 "+ "Server Todo List", description=authorTodo(authorSent), colour=discord.Colour.random())
-      await message.channel.send(content=None, embed=embedTodos)
+      embedTodos = discord.Embed(title=f"📝 {user}'s Todo List", description=authorTodo(authorSent), colour=discord.Colour.random())
+      await ctx.channel.send(content=None, embed=embedTodos)
 
-    if message.content.lower() == "$hw clear":
-      authorSent =  str(message.guild.id) #Author ID
+      
+    elif args[0] == 'clear':
+      authorSent = str(ctx.guild.id) #Server ID
+      authorCleared = str(ctx.author.id) #User ID
+      user = ctx.guild.name #username
+      userCleared = await bot.fetch_user(authorCleared) #Server name
       removeAllTodo(authorSent)
-      embedClearedTodo = discord.Embed(title="✅ Your Server's Todo List has been cleared", colour=discord.Colour.green())
-      await message.channel.send(content=None, embed=embedClearedTodo)
+      embedClearedTodo = discord.Embed(title=f"✅ {user}'s Todo List has been cleared", colour=discord.Colour.green())
+      embedClearedTodo.set_author(name=userCleared, icon_url=userCleared.avatar_url)
+      await ctx.channel.send(content=None, embed=embedClearedTodo)
+    
 #json.dump(allTodo, open("todolist.txt",'w')) // If you use a text file
-
+    #@bot.command()
     #FIRST TIME SETUP: Setting Study Voice channel
-    if message.content.startswith('$set'):
-      authorSent = str(message.author.id)
-
-
+    #async def set(ctx,*args):
+    #if len(args) == 0:
+    #await 
+    #
 # Under Development - Voice Channel Logging
-@client.event
+@bot.event
 async def on_voice_state_update(member, before, after):
+    global usersInCall
     if before.channel is None and after.channel is not None:
         if after.channel.id == 857610586577436682:
-          vcName = client.get_channel(857610586577436682)
-          await member.guild.system_channel.send(f"User Joined {vcName}")
+          vcName = bot.get_channel(857610586577436682)
+          await member.guild.system_channel.send(f"{member}Joined {vcName}")
           members = vcName.members #finds members connected to the channel
 
-          memids = [] #(list)
+          usersInCall = [] #(list)
           for member in members:
-            memids.append(member.id)
+            usersInCall.append(member.id)
 
-            print(memids) #print info
-            await member.guild.system_channel.send(memids)
+            print(usersInCall) #print info
     if before.channel is not None and after.channel is None:
+        
         if before.channel.id == 857610586577436682:
-          vcName = client.get_channel(857610586577436682)
-          await member.guild.system_channel.send(f"User Left {vcName}")
+          vcName = bot.get_channel(857610586577436682)
+          await member.guild.system_channel.send(f"{member} Left {vcName}")
           members = vcName.members #finds members connected to the channel
-
-          memids = [] #(list)
+          usersRemaining = [] #(list)
           for member in members:
-            memids.append(member.id)
+            usersRemaining.append(member.id)
+            print(userRemaining) #print info
 
-            print(memids) #print info
-            await member.guild.system_channel.send(memids)
-  
 keep_alive()
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
